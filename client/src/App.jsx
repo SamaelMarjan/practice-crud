6d8d15ad24eca6f347a7fdbad8a80e6be5766fd3
samaelmarjan <samaaelmarjan@gmail.com>
2024-05-15 14:38:28 +0600

    warap pages in route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import CreateBlog from "./pages/CreateBlog";
+import UpdateBlog from "./pages/UpdateBlog";
+import DeleteBlog from "./pages/DeleteBlog";
+import Error from "./pages/Error";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/create" element={<CreateBlog />} />
+      <Route path="/edit" element={<UpdateBlog />} />
+      <Route path="/delete" element={<DeleteBlog />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
 };
 
 export default App;
